634bed900da1c859f8c530e185d803926b8a037a
afayou <1147753060@qq.com>
2019-02-03 02:13:43 +0800

    Initial commit

--- 4_Convolutional-Neural-Networks.py
+++ 4_Convolutional-Neural-Networks.py
@@ -66,11 +66,11 @@ correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 tf.global_variables_initializer().run()
-for i in range(20000):
+for i in range(200000):
     batch = mnist.train.next_batch(100)
     if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g" % (i, train_accuracy))
+        print("step %d, training accuracy %f" % (i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob:1.0}))
+print("test accuracy %f" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob:1.0}))
