52bb987e5169524f88c84addd65c006577ebc5e3
afayou <1147753060@qq.com>
2019-02-14 05:01:50 +0800

    Initial commit

---
+++ 6_AlexNet.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import math
+import time
+import tensorflow as tf
+
+batch_size = 32
+num_batches = 100
+
+# 定义一个用来显示每一层结构的函数
+def print_activations(t):
+    print(t.op.name, '', t.get_shape().as_lsit())
+
+# 定义inference，接受image, 定义第一个卷积层
+def inference(images):
+    parameters = []
+
+    with tf.name_scope('conv1') as scope:
+        kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 64], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(images, kernel, [1, 4, 4, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(bias, name=scope)
+        print_activations(conv1)
+        parameters += [kernel, biases]
+
+        # 在第一个卷积层后添加LRN层和最大池化层
+        lrn1 = tf.nn.lrn(conv1, 4, bias=1.0, alpha=0.001/9, beta=0.75, name='lrn1')
+        pool1 = tf.nn.max_pool(lrn1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool1')
+
+    print_activations(pool1)
+
+    # 构建第二个卷积层
+    with tf.name_scope('conv2') as scope:
+        kernel = tf.Variable(tf.truncated_normal([5, 5, 64, 192], dtype=tf.float32, stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[192], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope)
+        parameters += [kernel, biases]
+
+    print_activations(conv2)
+
